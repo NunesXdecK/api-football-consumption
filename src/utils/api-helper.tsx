faab045c6be5595083ca2b87579f628bbe1d34f6
José Neto <zswnunes@hotmail.com>
2023-05-21 19:09:39 -0400

    feat: add helper

---
+++ src/utils/api-helper.tsx
@@ -0,0 +1,10 @@
+export const ApiHelper = {
+    url: "https://v3.football.api-sports.io/",
+    status: "status",
+    teams: "teams",
+    leagues: "leagues",
+    seasons: "leagues/seasons",
+    countries: "countries",
+    teamsStatics: "teams/statistics",
+    palyers: "players",
+}
